1aed9de1b8fadb9187a0f7a5940860705d3dc5d5
Dustin Irving <dustin.irving@gmail.com>
2020-08-22 15:17:04 -0400

    Next js Setup

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const About = () => (
+  <div>
+    <Navbar />
+    <h1>About Page</h1>
+    <p>All about the page...</p>
+  </div>
+);
+
+export default About;
